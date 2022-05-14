08df8ff9f84b440e6caceaa18b140db9b59c59ca
James Caleca <jacaleca2@gmail.com>
2022-05-13 18:57:41 -0600

    Two joggers initial commit

---
+++ two-joggers/app.js
@@ -0,0 +1,19 @@
+// Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles, finds the number of laps that each jogger has to complete before they meet each other again, at the same time, at the start.
+
+// The function takes two arguments:
+
+// The length of Bob's lap (larger than 0)
+// The length of Charles' lap (larger than 0)
+
+// The function should return an array (Tuple<int, int> in C#) containing exactly two numbers:
+
+// The first number is the number of laps that Bob has to run
+// The second number is the number of laps that Charles has to run
+
+var nbrOfLaps = function (x, y) {
+    return [0, 0];
+}
+
+console.log(nbrOfLaps(5, 3))
+console.log(nbrOfLaps(4, 6))
+console.log(nbrOfLaps(5, 5))
