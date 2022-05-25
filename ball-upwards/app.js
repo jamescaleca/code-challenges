d2293c1e22f8542095bf9d81213c07926c1fd03b
James Caleca <jacaleca2@gmail.com>
2022-05-25 14:15:03 -0600

    Ball upawrds solution

--- ball-upwards/app.js
+++ ball-upwards/app.js
@@ -13,5 +13,10 @@
 // The maximum height recorded by the device is not necessarily the maximum height reached by the ball.
 
 function maxBall(v0) {
-    // your code
-}
+    const vInMs = v0 / 3.6
+    const g = 9.81
+    const t = vInMs / g
+    const closestT = t.toFixed(1) * 10
+    
+    return closestT
+  }
